6d97c21ed4a022e8e53bdb72360d23f51c41eabf
Wolvahulk <wolvahulk@gmail.com>
2025-11-28 13:17:17 +0100

    changed color scheme, reworked code case and fixed misc. issues

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-const Ms = document.getElementById("Ms");
-const Mr = document.getElementById("Mr");
+const ms = document.getElementById("ms");
+const mr = document.getElementById("mr");
 const myButton = document.getElementById("myButton");
 const wynik = document.getElementById("wynik");
 
 myButton.onclick = function(){
     let answer;
-    let MsValue = Number(Ms.value);
-    let MrValue = Number(Mr.value);
+    let msValue = Number(ms.value);
+    let mrValue = Number(mr.value);
 
-    if(MsValue > MrValue && MsValue > 0 && MrValue > 0){
+    if(msValue > mrValue && msValue > 0 && mrValue > 0){
         window.alert("Masa substancji rozpuszczonej nie może być większa niż masa całego roztworu!");
     }
-    else if(MsValue > 0 && MrValue > 0){
-        answer = MsValue / MrValue * 100;
-        wynik.textContent = answer + "%";
+    else if(msValue > 0 && mrValue > 0){
+        answer = msValue / mrValue * 100;
+        wynik.textContent = answer.toFixed(2) + "%";
     }
     else{
         window.alert("Proszę podać prawidłowe liczby!");
     }
 }
 
-const Spr = document.getElementById("Spr");
-const Mr2 = document.getElementById("Mr2");
+const spr = document.getElementById("spr");
+const mr2 = document.getElementById("mr2");
 const myButton2 = document.getElementById("myButton2");
 const wynik2 = document.getElementById("wynik2");
 
     myButton2.onclick = function(){
         let answer;
-        let SprValue = Number(Spr.value);
-        let Mr2Value = Number(Mr2.value);
+        let sprValue = Number(spr.value);
+        let mr2Value = Number(mr2.value);
 
-        if(SprValue === 0){
+        if(sprValue === 0){
             window.alert("Stężenie procentowe nie może wynosić 0!");
         }
-        else if(SprValue <= 100 && Mr2Value > 0){
-            answer = SprValue / 100 * Mr2Value;
-            wynik2.textContent = answer + "g";
+        else if(sprValue <= 100 && mr2Value > 0){
+            answer = sprValue / 100 * mr2Value;
+            wynik2.textContent = answer.toFixed(2) + "g";
         }
         else{
             window.alert("Proszę podać właściwe procenty/gramy!");
         }
     }
 
-const Spr2 = document.getElementById("Spr2");
-const Ms2 = document.getElementById("Ms2");
+const spr2 = document.getElementById("spr2");
+const ms2 = document.getElementById("ms2");
 const myButton3 = document.getElementById("myButton3");
 const wyinik3 = document.getElementById("wynik3");
 
     myButton3.onclick = function(){
         let answer;
-        let Spr2Value = Number(Spr2.value);
-        let Ms2Value = Number(Ms2.value);
+        let spr2Value = Number(spr2.value);
+        let ms2Value = Number(ms2.value);
 
-        if(Spr2Value === 0){
+        if(spr2Value === 0){
             window.alert("Stężenie procentowe nie może wynosić 0!")
         }
-        else if(Spr2Value <= 100 && Ms2Value > 0){
-            answer = Ms2Value * 100 / Spr2Value;
-            wyinik3.textContent = answer + "g";
+        else if(spr2Value <= 100 && ms2Value > 0){
+            answer = ms2Value * 100 / spr2Value;
+            wyinik3.textContent = answer.toFixed(2) + "g";
         }
         else{
             window.alert("Proszę podać właściwe procenty/gramy!");
